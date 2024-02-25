0bbaf3ef113828a59a5b27b33b2d00a85a37b27b
潘静怡 <1003346758@qq.com>
2024-02-25 17:09:36 +0800

    fix: pluginify function param

--- packages/core/src/index.js
+++ packages/core/src/index.js
@@ -2,8 +2,12 @@ import { AxiosPluginify } from './pluginify/axios.js'
 import { FetchPluginify } from './pluginify/fetch.js'
 import { Plugin } from './pluginify/core.js'
 
-export function pluginify(instance) {
-  return instance ? new AxiosPluginify(instance) : new FetchPluginify()
+export function pluginify(instance, config) {
+  const funName = instance.toString().match(/function\s*([^(]*)\(/)[1]
+  if(funName === 'fetch'){
+    return new FetchPluginify(config)
+  }
+  return new AxiosPluginify(instance, config)
 }
 
 export { Plugin }
--- playground/src/App.vue
+++ playground/src/App.vue
@@ -11,7 +11,7 @@ const axiosInstance = pluginify(axios)
 axiosInstance.get('http://localhost:3000/posts')
   .then(res => console.log('axios res', res))
 
-const fetchInstance = pluginify()
+const fetchInstance = pluginify(fetch)
                         .use(new retryPlugin(), new cachePlugin())
                         .generate()
 
